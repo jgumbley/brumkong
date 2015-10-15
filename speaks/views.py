2881dd3572fc34ddec7913eacfeadda6dfaaf04b
Jim Gumbley <jgumbley@thoughtworks.com>
2015-10-15 22:06:00 +0100

    changes to support a simple template

--- speaks/views.py
+++ speaks/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.template.loader import get_template
 
 
 def index(request):
-    return HttpResponse("Yo bliar.")
+    return HttpResponse(get_template("frontpage.j2.html").render())
