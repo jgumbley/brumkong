5a1159f4955524c7373f159a9d27f2d605bdf644
Jim Gumbley <jgumbley@thoughtworks.com>
2015-12-21 13:31:48 +0000

    flake8

--- speaks/views.py
+++ speaks/views.py
@@ -5,6 +5,6 @@ from django.template.loader import get_template
 def index(request):
     return HttpResponse(get_template("frontpage.j2.html").render())
 
+
 def buildings(request):
     return HttpResponse(get_template("buildings.j2.html").render())
-
