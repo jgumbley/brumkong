04d539ce8853d56f12b641c4f2527ab85d1314f1
Jim Gumbley <jgumbley@thoughtworks.com>
2015-12-21 13:10:06 +0000

    thinking about a buildings section

--- speaks/views.py
+++ speaks/views.py
@@ -4,3 +4,7 @@ from django.template.loader import get_template
 
 def index(request):
     return HttpResponse(get_template("frontpage.j2.html").render())
+
+def buildings(request):
+    return HttpResponse(get_template("buildings.j2.html").render())
+
