f7658c3ccdd0b668f9bac737ca14ca6d995e5081
Jim Gumbley <jgumbley@thoughtworks.com>
2015-12-25 11:45:55 +0000

    dynamically set the date at the top of the page

--- speaks/views.py
+++ speaks/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from django.template.loader import get_template
 from speaks.models import Tweet
+import datetime
+
+
+def nice_date():
+    """Move this please
+    """
+    today = datetime.date.today()
+    # todo add th, nd, st etc.
+    return today.strftime('%d %B %Y')
 
 
 def index(request):
     tweets = Tweet.objects.all()
-    date = "25th December 2015"
+    date = nice_date()
     return HttpResponse(get_template("frontpage.j2.html")
                         .render({"tweets": tweets,
                                  "date": date})
